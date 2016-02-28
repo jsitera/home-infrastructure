f32a08e039fe490e491116c57ff467d62e2439cb
jsitera <sitera@civ.zcu.cz>
2016-02-28 22:00:51 +0100

    Comments and correction of errors.

--- setup-OS/first-setup.sh
+++ setup-OS/first-setup.sh
@@ -6,10 +6,11 @@
 
 # install session fallback instead of unity
 apt-get install gnome-session-fallback
+# log out and choose gnome(compiz) as a session
 
 # move buttons to the right side
 # gconf-editor
-gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize, close'
+gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize,close'
 
 # UFW firewall enable and setup
 
