865152ba2ae83a928bb91684404136a5e06563dd
Jiri Sitera <sitera@civ.zcu.cz>
2016-10-08 16:18:27 +0200

    Support for two dimmers, tested with my hardware.

--- esp8266/actor-LED-dimmer/actor-LED-dimmer.ino
+++ esp8266/actor-LED-dimmer/actor-LED-dimmer.ino
@@ -1,5 +1,5 @@
 // dimmer with two button control
-// TODO supports two separate dimmers
+// supports two separate dimmers
 // first version without OpenHAB connectivity
 
 // library for button management using callbacks 
@@ -18,11 +18,13 @@
 //----- configuration --------------------------------------------
 // buttons and outputs
 PushButton button1_up = PushButton(4, ENABLE_INTERNAL_PULLUP);
-PushButton button1_down = PushButton(5, ENABLE_INTERNAL_PULLUP);
+PushButton button1_down = PushButton(0, ENABLE_INTERNAL_PULLUP);
+PushButton button2_up = PushButton(2, ENABLE_INTERNAL_PULLUP);
+PushButton button2_down = PushButton(14, ENABLE_INTERNAL_PULLUP);
+// don't use gpio15 (build-in pull-down resistor needed for bootup process)
 
-
-#define output1 0
-#define output2 2
+#define output1 5
+#define output2 3
 
 // brightness is a byte 0-255
 #define firstBrightness 200 // initial value of remembered brightness
@@ -40,6 +42,7 @@ PushButton button1_down = PushButton(5, ENABLE_INTERNAL_PULLUP);
 //----------------------------------------------------------------
 
 Ticker fancyOff1; // Ticker.h object to call a callback 
+Ticker fancyOff2; // Ticker.h object to call a callback 
 
 void setup() {
   Serial.begin(115200);
@@ -48,19 +51,22 @@ void setup() {
   // When the button is released
   button1_up.onRelease(onButtonReleased);
   button1_down.onRelease(onButtonReleased);
+  button2_up.onRelease(onButtonReleased);
+  button2_down.onRelease(onButtonReleased);
   
   // Once the button has been held for xxx ms call onButtonHeld. Call it again every yyy ms until it is let go
   button1_up.onHoldRepeat(holdInterval, fadeInterval, onButtonHeld);
   button1_down.onHoldRepeat(holdInterval, fadeInterval, onButtonHeld);
+  button2_up.onHoldRepeat(holdInterval, fadeInterval, onButtonHeld);
+  button2_down.onHoldRepeat(holdInterval, fadeInterval, onButtonHeld);
 
   pinMode(output1, OUTPUT);
-
+  pinMode(output2, OUTPUT);
 
   analogWriteRange(255); // default on ESP is 1023
   //analogWriteFreq(600); //??? 1000 Hz is default
-  analogWrite(output1,0);
 
-
+  setOutput(0, 0);
   
   Serial.println("Starting...");
 }
@@ -77,6 +83,8 @@ void loop() {
   // Check the state of the buttons
   button1_up.update();
   button1_down.update();
+  button2_up.update();
+  button2_down.update();
 
   // Ticker.h doesn't need to call (it is attached to hardware interupt)
 }
@@ -97,14 +105,27 @@ void onButtonReleased(Button& btn, uint16_t duration){
         //no brightness1 = 0; because the actual switch off will be provided by fancyOff callback
         fancyOff1.attach_ms(fancyOffInterval, fancyOff1CB);
       }  
-    Serial.print("1 down ");
+      Serial.print("1 down ");
+    } else if (btn.is(button2_up)){  // switch on 
+      brightness2 = rememberedBrightness2;
+      fancyOff2.detach();; // stop the fancyOff if we are switching on early
+      Serial.print("2 up ");
+    } else if (btn.is(button2_down)){ // switch off
+      if (brightness2 > 0){
+        rememberedBrightness2 = brightness2; // remember last brightness before switching off
+        //no brightness2 = 0; because the actual switch off will be provided by fancyOff callback
+        fancyOff2.attach_ms(fancyOffInterval, fancyOff2CB);
+      }  
+      Serial.print("2 down ");
     }
   }
 
-  analogWrite(output1, brightness1);
+  setOutput(brightness1, brightness2);
   
-  Serial.print("buttonReleased end brightness: ");
-  Serial.println(brightness1);
+  Serial.print("buttonReleased end brightness: 1:");
+  Serial.print(brightness1);
+  Serial.print(" 2:");
+  Serial.println(brightness2);
 }
 
 // 
@@ -125,18 +146,30 @@ void onButtonHeld(Button& btn, uint16_t duration, uint16_t repeatCount){
     if (brightness1 > maxBrightness) {
       brightness1 = maxBrightness;
     }
-  } else if (btn.is(button1_down)){ // fade down
+  } else if (btn.is(button1_down)){ // fade down, fading down to zero is like switching off but without any change to remembered brightness
     Serial.print("1 fade down ");
     if ((brightness1 - fadeStep) >= minBrightness) {
       brightness1 -= fadeStep;
+    } 
+  } else if (btn.is(button2_up)){  // fade up
+    Serial.print("2 fade up ");
+    brightness2 += fadeStep;
+    if (brightness2 > maxBrightness) {
+      brightness2 = maxBrightness;
     }
-    // fading down to zero is like switching off but without any change to remembered brightness
+  } else if (btn.is(button2_down)){ // fade down, fading down to zero is like switching off but without any change to remembered brightness
+    Serial.print("2 fade down ");
+    if ((brightness2 - fadeStep) >= minBrightness) {
+      brightness2 -= fadeStep;
+    }
   }
 
-  analogWrite(output1, brightness1);
-
-  Serial.print("buttonHeld end brightness: ");
-  Serial.println(brightness1);
+  setOutput(brightness1, brightness2);
+    
+  Serial.print("buttonHeld end brightness: 1:");
+  Serial.print(brightness1);
+  Serial.print(" 2:");
+  Serial.println(brightness2);
 
 }
 
@@ -148,6 +181,26 @@ void fancyOff1CB() {
     brightness1 = 0;
     fancyOff1.detach(); //stop calling this callback
   }
+  setOutput(brightness1, brightness2);
+}
+
+void fancyOff2CB() {
+  if ((brightness2 - fancyOffStep) > 0 ) {
+    brightness2 -= fancyOffStep;
+  } else {
+    brightness2 = 0;
+    fancyOff2.detach(); //stop calling this callback
+  }
+  setOutput(brightness1, brightness2);
+}
+
+void setOutput(int brightness1, int brightness2) {
   analogWrite(output1, brightness1);
+  analogWrite(output2, brightness2);
+  
+  Serial.print("setOutput: 1:");
+  Serial.print(brightness1);
+  Serial.print(" 2:");
+  Serial.println(brightness2);
 }
 
